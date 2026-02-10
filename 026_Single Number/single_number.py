eaecd2c7b18b35ce3b1579dacaf54a8fc1ae49ce
mahider yizengaw <124761678+Finotmah@users.noreply.github.com>
2026-02-09 20:37:44 -0800

    Refactor singleNumber method for efficiency

--- 026_Single Number/single_number.py
+++ 026_Single Number/single_number.py
@@ -1,10 +1,4 @@
 class Solution(object):
     def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        result = 0
-        for n in nums:
-            result ^= n  
-        return result
+        
+        return 2 * sum(set(nums)) - sum(nums)
