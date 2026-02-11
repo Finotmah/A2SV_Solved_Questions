a5dec9ad64867b004aa88ccef2d35925701f608e
mahider yizengaw <124761678+Finotmah@users.noreply.github.com>
2026-02-11 03:23:50 -0800

    Implement minSteps method for anagram conversion
    Removed spiralOrder method and implemented minSteps method to calculate minimum steps to make two strings anagrams.

--- 040_Minimum Number of Steps to Make Two Strings Anagram/Minimum Number of Steps to Make Two Strings Anagram.py
+++ 040_Minimum Number of Steps to Make Two Strings Anagram/Minimum Number of Steps to Make Two Strings Anagram.py
@@ -1,37 +1,29 @@
+from collections import Counter
 class Solution(object):
-    def spiralOrder(self, matrix):
-        m = len(matrix)
-        n = len(matrix[0])
+    def minSteps(self, s, t):
+        
+        s_count = Counter(s)
+        t_count = Counter(t)
+        min_ops = 0
+        seen = set()
+        
+        for char in s:
+            if char in seen:
+                continue
+                
+            ops = s_count[char] - t_count[char]
+            if ops > 0:
+                min_ops += ops
 
-        left , right = 0, n - 1
-        top , bottom = 0, m - 1
-        result = []
+            seen.add(char)
+        return min_ops
 
-        while left <= right and top <= bottom:
-
-            for i in range(left,right + 1):
-                result.append(matrix[top][i])
-            top += 1
-
-            for i in range(top, bottom + 1):
-                result.append(matrix[i][right])
-            right -= 1
-
-            if not(left  <= right and top <= bottom):
-                break
             
-            for i in range(right,left -1, -1):
-                result.append(matrix[bottom][i])
-            bottom -= 1
             
-            for i in range(bottom,top - 1, -1):
-                result.append(matrix[i][left])
 
-            left += 1
+
+
+        
+
             
-        return result
-
-
-
-    
-
+        
