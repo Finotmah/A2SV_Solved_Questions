7eb822db0dfbaac31dfd7f813a24f544a3cc1b92
mahider yizengaw <124761678+Finotmah@users.noreply.github.com>
2026-02-12 06:32:34 -0800

    Add setZeroes method to set matrix rows and cols to zero
    Implement setZeroes method to modify matrix in-place.

---
+++ 056_Set Matrix Zeroes/set matrix seroes.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        row_set = set()
+        col_set = set()
+
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c] == 0:
+                    row_set.add(r)
+                    col_set.add(c)
+        for r in range(rows):
+            for c in range(cols):
+                if r in row_set or c in col_set:
+                    matrix[r][c] = 0
+        
+        return matrix
+
+        
