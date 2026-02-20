b39386e4f3b24c7b4ad53ca93f6e25936ef4cfb8
mahider yizengaw <124761678+Finotmah@users.noreply.github.com>
2026-02-20 06:02:19 -0800

    Fix variable name from 'min' to 'mins'

--- 077_Majority Element2/Majority Element2.py
+++ 077_Majority Element2/Majority Element2.py
@@ -3,11 +3,11 @@ from collections import Counter
 class Solution(object):
     def majorityElement(self, nums):
         counts = Counter(nums)
-        min = len(nums) // 3
+        mins = len(nums) // 3
         result = []
         
         for num in counts:
-            if counts[num] > min:
+            if counts[num] > mins:
                 result.append(num)
         
         return result
