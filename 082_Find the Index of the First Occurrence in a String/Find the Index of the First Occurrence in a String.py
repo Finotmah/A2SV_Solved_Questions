02abaf69d2c361d53f78bc0a97d0be22213c1800
mahider yizengaw <124761678+Finotmah@users.noreply.github.com>
2026-02-24 12:39:15 -0800

    Refactor strStr method with sliding window approach
    Refactored strStr method to implement a sliding window approach for finding the first occurrence of a substring.

--- 082_Find the Index of the First Occurrence in a String/Find the Index of the First Occurrence in a String.py
+++ 082_Find the Index of the First Occurrence in a String/Find the Index of the First Occurrence in a String.py
@@ -5,7 +5,21 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
+        s1 = len(needle)
+        s2 = len(haystack)
+        l = 0
+        r = len(needle)
+        w = haystack[:s1]
         if needle in haystack:
-            return haystack.index(needle)
+            if w == needle:
+                return 0
+            else:
+                for i in range(s1,s2):
+                    l += 1
+                    r += 1
+                    w = haystack[l:r]
+                    if w == needle:
+                        return l
         else:
-            return -1
+            return -1           
+
