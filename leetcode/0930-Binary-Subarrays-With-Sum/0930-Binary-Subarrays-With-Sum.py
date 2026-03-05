c7634f8ab295da44f248e0a628858651a1f087ff
mahider yizengaw <124761678+Finotmah@users.noreply.github.com>
2026-03-05 08:40:43 -0800

    Add solution for Binary Subarrays With Sum

---
+++ leetcode/0930-Binary-Subarrays-With-Sum/0930-Binary-Subarrays-With-Sum.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def numSubarraysWithSum(self, nums, goal):
+        """
+        :type nums: List[int]
+        :type goal: int
+        :rtype: int
+        """
+        n = len(nums)
+        sums = 0
+        num_sub = 0
+        frq = dict()
+        for num in nums:
+            sums += num
+            if sums == goal:
+                num_sub += 1
+            
+            if sums - goal in frq:
+                num_sub += frq[sums - goal]
+            
+            frq[sums] = frq.get(sums,0) + 1
+        
+        return num_sub
